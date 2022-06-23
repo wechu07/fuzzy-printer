8362c028d9a5f0d49f4e4e2d57f9227d2a626a1e
wechu07 <wechuli017@gmail.com>
2022-06-23 19:10:30 +0300

    logic

--- print.py
+++ print.py
@@ -5,7 +5,7 @@ list_of_names = []
 def cleanup_data():
     with open("names-data.txt") as file:
         for line in file:
-            list_of_names.append(line.strip());
+            list_of_names.append(line.strip())
 
 def generate_certificates():
     for name in list_of_names:
